1192a0e861f87f97dd0ef945ad0964607008f893
wmr1212 <s21260034@nucba.ac.jp>
2022-02-07 02:24:09 +0900

    新規登録・ログイン・ログアウト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :purchase_histories
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :family_name_kana, presence: true
+  validates :birth_date, presence: true
 end
