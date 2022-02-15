a10125ec860adfb88a21e5214defd410cf12583d
wmr1212 <s21260034@nucba.ac.jp>
2022-02-16 03:52:53 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,5 +6,13 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :shipping_area
+  belongs_to :trading_status
+
+  validates :name, :explanation, :price, presence: true
+
+  validates :category_id, :condition_id, :delivery_charge_id, :shipping_area_id, :trading_status_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
