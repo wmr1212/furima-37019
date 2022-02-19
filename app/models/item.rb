83a95ed3184bded95529c90d6ea12e86f372176b
wmr1212 <s21260034@nucba.ac.jp>
2022-02-19 11:41:14 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :trading_status
 
-  validates :name, :explanation, :price, presence: true
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true, numericality: { 
+    less_than_or_equal_to: 9999999,
+    greater_than_or_equal_to: 300
+    }
 
   validates :category_id, :condition_id, :delivery_charge_id, :shipping_area_id, :trading_status_id, numericality: { other_than: 1 , message: "can't be blank"}
 
